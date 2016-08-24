7841a401cbbbd60f0a628c1a74e3957cab9cfd65
Nguyen Vo <binhnguyenvo@gmail.com>
2016-08-24 12:19:25 +0700

    Day len tu local ne

---
+++ Moine/DataPoint.java
@@ -0,0 +1,40 @@
+
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(propOrder = { "songId", "time"})
+public class DataPoint {
+
+	private int time;
+
+	private String songId;
+	
+	public DataPoint()
+	{
+		this("", 0);
+	}
+
+	public DataPoint(String songId, int time) {
+		this.setSongId(songId);
+		this.setTime(time);
+	}
+
+	@XmlElement(name="SongId")
+	public String getSongId() {
+		return this.songId;
+	}
+	
+	public void setSongId(String songId) {
+		this.songId = songId;
+	}
+
+	@XmlElement(name="Time")
+	public int getTime() {
+		return this.time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+}
